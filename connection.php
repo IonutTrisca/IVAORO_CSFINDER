8ac3824c6e9cccfa42523b0dfd1ef244d854bd49
Ionut Trisca <trisca46@gmail.com>
2019-06-30 16:15:35 +0300

    adjusted the names of the vars

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 /* This file is used to connect to the database */
 
-DEFINE ('DB_USER', 'root'); //database username
-DEFINE ('DB_PASS', ''); //user password
-DEFINE ('DB_HOST', 'localhost'); //database host
-DEFINE ('DB_NAME', 'local testing'); //database name
+DEFINE ('DB_USER', 'yourUsername'); //database username
+DEFINE ('DB_PASS', 'yourPassword'); //user password
+DEFINE ('DB_HOST', 'yourHost'); //database host
+DEFINE ('DB_NAME', 'yourDBName'); //database name
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
 OR die('Could not connect to MySQL ' . mysqli_connect_error());
