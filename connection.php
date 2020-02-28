b87f66bef3bfa5f63fe463ad4d63e17ee71078ad
IonutTrisca <trisca46@gmail.com>
2020-02-28 23:14:15 +0200

    double change

--- connection.php
+++ connection.php
@@ -6,8 +6,6 @@ DEFINE ('DB_PASS', 'yourPassword'); //user password
 DEFINE ('DB_HOST', 'yourHost'); //database host
 DEFINE ('DB_NAME', 'yourDBName'); //database name
 
-
-
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
 OR die('Could not connect to MySQL ' . mysqli_connect_error());
 ?>
